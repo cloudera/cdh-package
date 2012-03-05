markgrover/bigtop-rvs

BIGTOP-417. webapps shipped by bigtop components should be using a system-provided Tomcat
git-svn-id: https://svn.apache.org/repos/asf/incubator/bigtop/branches/hadoop-0.23@1296379 13f79535-47bb-0310-9956-ffa450edef68

bigtop-packages/src/common/bigtop-tomcat/install_tomcat.sh
@@ -1,0 +1,91 @@
+#!/bin/bash
+
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+usage() {
+  echo "
+usage: $0 <options>
+  Required not-so-options:
+     --build-dir=DIR             path to Whirr dist.dir
+     --prefix=PREFIX             path to install into
+
+  Optional options:
+     --doc-dir=DIR               path to install docs into [/usr/share/doc/whirr]
+     --lib-dir=DIR               path to install Whirr home [/usr/lib/whirr]
+  "
+  exit 1
+}
+
+OPTS=$(getopt \
+  -n $0 \
+  -o '' \
+  -l 'prefix:' \
+  -l 'doc-dir:' \
+  -l 'lib-dir:' \
+  -l 'build-dir:' -- "$@")
+
+if [ $? != 0 ] ; then
+    usage
+fi
+
+eval set -- "$OPTS"
+while true ; do
+    case "$1" in
+        --prefix)
+        PREFIX=$2 ; shift 2
+        ;;
+        --build-dir)
+        BUILD_DIR=$2 ; shift 2
+        ;;
+        --doc-dir)
+        DOC_DIR=$2 ; shift 2
+        ;;
+        --lib-dir)
+        LIB_DIR=$2 ; shift 2
+        ;;
+        --)
+        shift ; break
+        ;;
+        *)
+        echo "Unknown option: $1"
+        usage
+        exit 1
+        ;;
+    esac
+done
+
+for var in PREFIX BUILD_DIR ; do
+  if [ -z "$(eval "echo \$$var")" ]; then
+    echo Missing param: $var
+    usage
+  fi
+done
+
+MAN_DIR=$PREFIX/usr/share/man/man1
+DOC_DIR=${DOC_DIR:-$PREFIX/usr/share/doc/bigtop-tomcat}
+LIB_DIR=${LIB_DIR:-$PREFIX/usr/lib/bigtop-tomcat}
+
+# First we'll move everything into lib
+install -d -m 0755 $LIB_DIR
+(cd $BUILD_DIR && tar -cf - .) | (cd $LIB_DIR && tar -xf -)
+
+rm -rf $LIB_DIR/webapps
+mv -f $LIB_DIR/conf $LIB_DIR/conf.template
+
+install -d -m 0755 $DOC_DIR
+mv $LIB_DIR/{RUNNING.txt,RELEASE-NOTES,NOTICE,LICENSE} $DOC_DIR

bigtop-packages/src/rpm/bigtop-tomcat/SPECS/bigtop-tomcat.spec
@@ -1,0 +1,59 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+%define lib_tomcat %{_usr}/lib/%{name}
+
+%if  %{?suse_version:1}0
+  %define doc_tomcat %{_docdir}/%{name}
+%else
+  %define doc_tomcat %{_docdir}/%{name}-%{version}
+%endif
+
+Name: bigtop-tomcat
+Version: %{bigtop_tomcat_version}
+Release: %{bigtop_tomcat_release}
+Summary: Apache Tomcat
+URL: http://tomcat.apache.org/
+Group: Development/Libraries
+BuildArch: noarch
+Buildroot: %(mktemp -ud %{_tmppath}/%{name}-%{version}-%{release}-XXXXXX)
+License: ASL 2.0 
+Source0: apache-tomcat-%{bigtop_tomcat_base_version}.tar.gz
+Source1: do-component-build
+Source2: install_tomcat.sh
+
+%description 
+Apache Tomcat is an open source software implementation of the
+Java Servlet and JavaServer Pages technologies.
+
+%prep
+%setup -n apache-tomcat-%{bigtop_tomcat_base_version}-src
+
+%build
+bash %{SOURCE1}
+
+%install
+%__rm -rf $RPM_BUILD_ROOT
+bash %{SOURCE2} \
+          --build-dir=build \
+	  --doc-dir=$RPM_BUILD_ROOT%{doc_tomcat} \
+          --prefix=$RPM_BUILD_ROOT
+
+%files 
+%defattr(-,root,root)
+%attr(0755,root,root) %{lib_tomcat}
+%doc %{doc_tomcat}
+
+%changelog
+

cloudera/cdh-package

bigtop-packages/src/common/bigtop-tomcat/install_tomcat.sh
@@ -1,0 +1,93 @@
+#!/bin/bash
+
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+usage() {
+  echo "
+usage: $0 <options>
+  Required not-so-options:
+     --build-dir=DIR             path to Whirr dist.dir
+     --prefix=PREFIX             path to install into
+
+  Optional options:
+     --doc-dir=DIR               path to install docs into [/usr/share/doc/whirr]
+     --lib-dir=DIR               path to install Whirr home [/usr/lib/whirr]
+  "
+  exit 1
+}
+
+OPTS=$(getopt \
+  -n $0 \
+  -o '' \
+  -l 'prefix:' \
+  -l 'doc-dir:' \
+  -l 'lib-dir:' \
+  -l 'build-dir:' -- "$@")
+
+if [ $? != 0 ] ; then
+    usage
+fi
+
+eval set -- "$OPTS"
+while true ; do
+    case "$1" in
+        --prefix)
+        PREFIX=$2 ; shift 2
+        ;;
+        --build-dir)
+        BUILD_DIR=$2 ; shift 2
+        ;;
+        --doc-dir)
+        DOC_DIR=$2 ; shift 2
+        ;;
+        --lib-dir)
+        LIB_DIR=$2 ; shift 2
+        ;;
+        --)
+        shift ; break
+        ;;
+        *)
+        echo "Unknown option: $1"
+        usage
+        exit 1
+        ;;
+    esac
+done
+
+for var in PREFIX BUILD_DIR ; do
+  if [ -z "$(eval "echo \$$var")" ]; then
+    echo Missing param: $var
+    usage
+  fi
+done
+
+MAN_DIR=$PREFIX/usr/share/man/man1
+DOC_DIR=${DOC_DIR:-$PREFIX/usr/share/doc/bigtop-tomcat}
+LIB_DIR=${LIB_DIR:-$PREFIX/usr/lib/bigtop-tomcat}
+
+# First we'll move everything into lib
+install -d -m 0755 $LIB_DIR
+cp -r $BUILD_DIR/work $LIB_DIR/work
+cp -r $BUILD_DIR/temp $LIB_DIR/temp
+cp -r $BUILD_DIR/logs $LIB_DIR/logs
+cp -r $BUILD_DIR/lib  $LIB_DIR/lib
+cp -r $BUILD_DIR/conf $LIB_DIR/conf.template
+cp -r $BUILD_DIR/bin  $LIB_DIR/bin
+
+install -d -m 0755 $DOC_DIR
+cp $BUILD_DIR/{RUNNING.txt,RELEASE-NOTES,NOTICE,LICENSE} $DOC_DIR

bigtop-packages/src/rpm/bigtop-tomcat/SPECS/bigtop-tomcat.spec
@@ -1,0 +1,59 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+%define lib_tomcat %{_usr}/lib/%{name}
+
+%if  %{?suse_version:1}0
+  %define doc_tomcat %{_docdir}/%{name}
+%else
+  %define doc_tomcat %{_docdir}/%{name}-%{version}
+%endif
+
+Name: bigtop-tomcat
+Version: %{bigtop_tomcat_version}
+Release: %{bigtop_tomcat_release}
+Summary: Apache Tomcat
+URL: http://tomcat.apache.org/
+Group: Development/Libraries
+BuildArch: noarch
+Buildroot: %(mktemp -ud %{_tmppath}/%{name}-%{version}-%{release}-XXXXXX)
+License: ASL 2.0 
+Source0: bigtop-tomcat-%{bigtop_tomcat_patched_version}.tar.gz
+Source1: do-component-build
+Source2: install_tomcat.sh
+
+%description 
+Apache Tomcat is an open source software implementation of the
+Java Servlet and JavaServer Pages technologies.
+
+%prep
+%setup -n bigtop-tomcat-%{bigtop_tomcat_patched_version}
+
+%build
+env FULL_VERSION=%{bigtop_tomcat_patched_version} bash %{SOURCE1}
+
+%install
+%__rm -rf $RPM_BUILD_ROOT
+bash %{SOURCE2} \
+          --build-dir=build/bigtop-tomcat-%{bigtop_tomcat_patched_version} \
+	  --doc-dir=$RPM_BUILD_ROOT%{doc_tomcat} \
+          --prefix=$RPM_BUILD_ROOT
+
+%files 
+%defattr(-,root,root)
+%attr(0755,root,root) %{lib_tomcat}
+%doc %{doc_tomcat}
+
+%changelog
+