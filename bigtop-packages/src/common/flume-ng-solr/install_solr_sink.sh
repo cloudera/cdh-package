9ba133b76d184a0df24ec8551012661460b98ae6
Roman Shaposhnik <rvs@cloudera.com>
2012-11-20 17:24:35 -0800

    CDH-9111. create packaging for flume solr sink

---
+++ bigtop-packages/src/common/flume-ng-solr/install_solr_sink.sh
@@ -0,0 +1,112 @@
+#!/bin/sh
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -ex
+
+usage() {
+  echo "
+usage: $0 <options>
+  Required not-so-options:
+     --build-dir=DIR             path to flumedist.dir
+     --prefix=PREFIX             path to install into
+
+  Optional options:
+     --doc-dir=DIR               path to install docs into [/usr/share/doc/flume]
+     --flume-dir=DIR             path to install flume home [/usr/lib/flume]
+     --installed-lib-dir=DIR     path where lib-dir will end up on target system
+     ... [ see source for more similar options ]
+  "
+  exit 1
+}
+
+OPTS=$(getopt \
+  -n $0 \
+  -o '' \
+  -l 'prefix:' \
+  -l 'doc-dir:' \
+  -l 'doc-dir-prefix:' \
+  -l 'flume-dir:' \
+  -l 'installed-lib-dir:' \
+  -l 'build-dir:' -- "$@")
+
+if [ $? != 0 ] ; then
+    usage
+fi
+
+eval set -- "$OPTS"
+
+while true ; do
+    case "$1" in
+        --prefix)
+        PREFIX=$2 ; shift 2
+        ;;
+        --build-dir)
+        BUILD_DIR=$2 ; shift 2
+        ;;
+        --doc-dir)
+        DOC_DIR=$2 ; shift 2
+        ;;
+        --doc-dir-prefix)
+        DOC_DIR_PREFIX=$2 ; shift 2
+        ;;
+        --flume-dir)
+        FLUME_DIR=$2 ; shift 2
+        ;;
+        --installed-lib-dir)
+        INSTALLED_LIB_DIR=$2 ; shift 2
+        ;;
+        --)
+        shift ; break
+        ;;
+        *)
+        echo "Unknown option: $1"
+        usage
+        exit 1
+        ;;
+    esac
+done
+
+for var in PREFIX BUILD_DIR ; do
+  if [ -z "$(eval "echo \$$var")" ]; then
+    echo Missing param: $var
+    usage
+  fi
+done
+
+MAN_DIR=${MAN_DIR:-/usr/share/man/man1}
+DOC_DIR=${DOC_DIR:-/usr/share/doc/flume-ng}
+DOC_DIR_PREFIX=${DOC_DIR_PREFIX:-$PREFIX}
+FLUME_DIR=${FLUME_DIR:-/usr/lib/flume-ng}
+CONF_DIR=/etc/flume-ng/
+CONF_DIST_DIR=/etc/flume-ng/conf.dist/
+ETC_DIR=${ETC_DIR:-/etc/flume-ng}
+
+# Plugin jars
+install -d -m 0755 ${PREFIX}/${FLUME_DIR}/lib
+cp ${BUILD_DIR}/target/flume-ng-solr-sink-*.jar ${PREFIX}/${FLUME_DIR}/lib
+cp ${BUILD_DIR}/target/lib/*.jar ${PREFIX}/${FLUME_DIR}/lib
+
+# Sample (twitter) configs
+install -d -m 0755 ${PREFIX}/${CONF_DIST_DIR}
+cp ${BUILD_DIR}/src/test/resources/twitter-flume.conf  ${PREFIX}/${CONF_DIST_DIR}
+cp ${BUILD_DIR}/src/test/resources/tika-config.xml     ${PREFIX}/${CONF_DIST_DIR} 
+cp -r ${BUILD_DIR}/src/test/resources/solr/collection1 ${PREFIX}/${CONF_DIST_DIR}
+# FIXME: get org/apache/tika/mime/custom-mimetypes.xml onto classpath
+cp -r ${BUILD_DIR}/src/test/resources/org              ${PREFIX}/${CONF_DIST_DIR}
+
+# Cloudera specific
+#install -d -m 0755 $PREFIX/$FLUME_DIR/cloudera
+#cp cloudera/cdh_version.properties $PREFIX/$FLUME_DIR/cloudera/
---
+++ bigtop-packages/src/rpm/flume-ng-solr/SPECS/flume-ng-solr.spec
@@ -0,0 +1,81 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+%define etc_flume /etc/flume-ng/conf
+%define lib_flume /usr/lib/flume-ng
+
+%if  %{?suse_version:1}0
+
+# Only tested on openSUSE 11.4. le'ts update it for previous release when confirmed
+%if 0%{suse_version} > 1130
+%define suse_check \# Define an empty suse_check for compatibility with older sles
+%endif
+
+# SLES is more strict and check all symlinks point to valid path
+# But we do point to a hadoop jar which is not there at build time
+# (but would be at install time).
+# Since our package build system does not handle dependencies,
+# these symlink checks are deactivated
+%define __os_install_post \
+    %{suse_check} ; \
+    /usr/lib/rpm/brp-compress ; \
+    %{nil}
+
+%define doc_flume %{_docdir}/flume-ng
+%define alternatives_cmd update-alternatives
+%global initd_dir %{_sysconfdir}/rc.d
+
+%else
+
+%define doc_flume %{_docdir}/flume-ng-%{flume_ng_version}
+%define alternatives_cmd alternatives
+%global initd_dir %{_sysconfdir}/rc.d/init.d
+
+%endif
+
+
+
+Name: flume-ng-solr
+Version: %{flume_ng_solr_version}
+Release: %{flume_ng_solr_release}
+Summary: Flume NG Solr Sink 
+URL: http://flume.apache.org/
+Group: Development/Libraries
+Buildroot: %{_topdir}/INSTALL/%{name}-%{version}
+BuildArch: noarch
+License: APL2
+Source0: %{name}-%{flume_ng_solr_patched_version}.tar.gz
+Source1: do-component-build
+Source2: install_solr_sink.sh
+Requires: flume-ng
+
+%description 
+Flume NG Solr Sink
+
+%prep
+%setup -n %{name}-%{flume_ng_solr_patched_version}
+
+%build
+env FULL_VERSION=%{flume_ng_solr_patched_version} bash -x %{SOURCE1}
+
+%install
+%__rm -rf $RPM_BUILD_ROOT
+sh %{SOURCE2} \
+          --build-dir=$PWD \
+          --prefix=$RPM_BUILD_ROOT
+
+%files 
+%defattr(644,root,root,755)
+%config(noreplace) %{etc_flume}.dist/*
+%{lib_flume}/lib
