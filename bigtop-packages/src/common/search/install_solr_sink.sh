1609b5749e212cb1098045975c16f59c79de750d
Roman Shaposhnik <rvs@cloudera.com>
2013-05-08 09:01:18 -0700

    KITCHEN-BUILD. Adding Search packaging

---
+++ bigtop-packages/src/common/search/install_solr_sink.sh
@@ -0,0 +1,134 @@
+#!/bin/sh
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -ex
+
+usage() {
+  echo "
+usage: $0 <options>
+  Required not-so-options:
+     --build-dir=DIR             path to flumedist.dir
+     --prefix=PREFIX             path to install into
+
+  Optional options:
+     --doc-dir=DIR               path to install docs into [/usr/share/doc/flume]
+     --flume-dir=DIR             path to install flume home [/usr/lib/flume]
+     --installed-lib-dir=DIR     path where lib-dir will end up on target system
+     ... [ see source for more similar options ]
+  "
+  exit 1
+}
+
+OPTS=$(getopt \
+  -n $0 \
+  -o '' \
+  -l 'prefix:' \
+  -l 'doc-dir:' \
+  -l 'doc-dir-prefix:' \
+  -l 'flume-dir:' \
+  -l 'installed-lib-dir:' \
+  -l 'build-dir:' -- "$@")
+
+if [ $? != 0 ] ; then
+    usage
+fi
+
+eval set -- "$OPTS"
+
+while true ; do
+    case "$1" in
+        --prefix)
+        PREFIX=$2 ; shift 2
+        ;;
+        --build-dir)
+        BUILD_DIR=$2 ; shift 2
+        ;;
+        --doc-dir)
+        DOC_DIR=$2 ; shift 2
+        ;;
+        --doc-dir-prefix)
+        DOC_DIR_PREFIX=$2 ; shift 2
+        ;;
+        --flume-dir)
+        FLUME_DIR=$2 ; shift 2
+        ;;
+        --installed-lib-dir)
+        INSTALLED_LIB_DIR=$2 ; shift 2
+        ;;
+        --)
+        shift ; break
+        ;;
+        *)
+        echo "Unknown option: $1"
+        usage
+        exit 1
+        ;;
+    esac
+done
+
+for var in PREFIX BUILD_DIR ; do
+  if [ -z "$(eval "echo \$$var")" ]; then
+    echo Missing param: $var
+    usage
+  fi
+done
+
+LIB_DIR=${LIB_DIR:-/usr/lib/search}
+MAN_DIR=${MAN_DIR:-/usr/share/man/man1}
+DOC_DIR=${DOC_DIR:-/usr/share/doc/search}
+DOC_DIR_PREFIX=${DOC_DIR_PREFIX:-$PREFIX}
+FLUME_DIR=${FLUME_DIR:-/usr/lib/flume-ng}
+# FIXME: once solr-mr and core indexer go upstream we need to rationalize this
+SOLR_MR_DIR=${SOLR_MR_DIR:-/usr/lib/solr/contrib/mr}
+CONF_DIR=/etc/flume-ng/
+CONF_DIST_DIR=/etc/flume-ng/conf.dist/
+ETC_DIR=${ETC_DIR:-/etc/flume-ng}
+
+# Create the search package
+install -d -m 0755 ${PREFIX}/${LIB_DIR}/lib
+cp ${BUILD_DIR}/search-contrib/target/*.jar ${PREFIX}/${LIB_DIR}
+# cp ${BUILD_DIR}/search-warc-parser/target/*.jar ${PREFIX}/${LIB_DIR}
+cp ${BUILD_DIR}/search-mr/target/*.jar ${PREFIX}/${LIB_DIR}
+cp ${BUILD_DIR}/search-core/target/*.jar ${PREFIX}/${LIB_DIR}
+(cd ${PREFIX}/${LIB_DIR} ; rm -f *-tests.jar *-sources.jar)
+
+# Plugin jars
+install -d -m 0755 ${PREFIX}/${FLUME_DIR}/lib
+cp ${BUILD_DIR}/search-flume/target/*.jar ${PREFIX}/${FLUME_DIR}/lib
+cp ${BUILD_DIR}/search-flume/target/lib/*.jar ${PREFIX}/${FLUME_DIR}/lib
+cp ${BUILD_DIR}/search-contrib/target/*.jar ${PREFIX}/${FLUME_DIR}/lib
+(cd ${PREFIX}/${FLUME_DIR}/lib ; rm -f *-tests.jar *-sources.jar `ls flume-ng-*jar | grep -v flume-ng-solr`)
+# FIXME: this *really* needs to get fixed
+for f in avro commons-cli commons-io commons-lang guava jsr305 paranamer snappy-java zookeeper ; do
+  rm -f ${PREFIX}/${FLUME_DIR}/lib/${f}*
+done
+
+# FIXME: once solr-mr and core indexer go upstream we need to rationalize this
+install -d -m 0755 ${PREFIX}/${SOLR_MR_DIR}
+cp ${BUILD_DIR}/search-mr/target/*.jar ${PREFIX}/${SOLR_MR_DIR}
+
+# Sample (twitter) configs
+install -d -m 0755 ${PREFIX}/${DOC_DIR}
+cp -r ${BUILD_DIR}/samples ${PREFIX}/${DOC_DIR}/examples
+# FIXME: sed-away test specific stuff
+sed -i -e '/TestParser/d' ${PREFIX}/${DOC_DIR}/examples/solr-nrt/tika-config.xml
+sed -i -e '/agent.sinks.solrSink.collection.collection1.solr.home/s#^.*$#agent.sinks.solrSink.collection.collection1.solr.home = '${DOC_DIR}/examples/solr-nrt/collection1# \
+       -e '/agent.sinks.solrSink.tika.config/s#^.*$#agent.sinks.solrSink.tika.config = '${DOC_DIR}/examples/solr-nrt/tika-config.xml# \
+    ${PREFIX}/${DOC_DIR}/examples/solr-nrt/twitter-flume.conf
+
+# Cloudera specific
+#install -d -m 0755 $PREFIX/$FLUME_DIR/cloudera
+#cp cloudera/cdh_version.properties $PREFIX/$FLUME_DIR/cloudera/
---
+++ bigtop-packages/src/rpm/search/SPECS/search.spec
@@ -0,0 +1,105 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+%define etc_flume /etc/flume-ng/conf
+%define lib_flume /usr/lib/flume-ng
+%define lib_dir   /usr/lib/%{name}
+
+%if  %{?suse_version:1}0
+
+# Only tested on openSUSE 11.4. le'ts update it for previous release when confirmed
+%if 0%{suse_version} > 1130
+%define suse_check \# Define an empty suse_check for compatibility with older sles
+%endif
+
+# SLES is more strict and check all symlinks point to valid path
+# But we do point to a hadoop jar which is not there at build time
+# (but would be at install time).
+# Since our package build system does not handle dependencies,
+# these symlink checks are deactivated
+%define __os_install_post \
+    %{suse_check} ; \
+    /usr/lib/rpm/brp-compress ; \
+    %{nil}
+
+%define doc_dir %{_docdir}/%{name}
+%define alternatives_cmd update-alternatives
+%global initd_dir %{_sysconfdir}/rc.d
+
+%else
+
+%define doc_dir %{_docdir}/%{name}-%{version}
+%define alternatives_cmd alternatives
+%global initd_dir %{_sysconfdir}/rc.d/init.d
+
+%endif
+
+
+
+Name: search
+Version: %{search_version}
+Release: %{search_release}
+Summary: Cloudera Search Project 
+URL: http://www.cloudera.com/
+Group: Development/Libraries
+Buildroot: %{_topdir}/INSTALL/%{name}-%{version}
+BuildArch: noarch
+License: APL2
+Source0: search-%{search_patched_version}.tar.gz
+Source1: do-component-build
+Source2: install_solr_sink.sh
+Requires: bigtop-utils
+
+%description
+Cloudera Search Project
+
+%package -n flume-ng-solr
+Summary: Flume NG Solr Sink
+Group: Development/Libraries
+Requires: flume-ng
+
+%description -n flume-ng-solr
+Flume NG Solr Sink
+
+%prep
+%setup -n %{name}-%{search_patched_version}
+
+%build
+env FULL_VERSION=%{search_patched_version} bash -x %{SOURCE1}
+
+%install
+%__rm -rf $RPM_BUILD_ROOT
+sh %{SOURCE2} \
+          --build-dir=$PWD \
+          --doc-dir=%{doc_dir} \
+          --prefix=$RPM_BUILD_ROOT
+%files
+%{lib_dir}
+%{doc_dir}
+
+%files -n flume-ng-solr
+%defattr(644,root,root,755)
+%{lib_flume}/lib
+
+%package -n solr-mapreduce
+Summary: Solr mapreduce indexer
+Group: Development/Libraries
+Requires: hadoop-client, bigtop-utils
+
+%description -n solr-mapreduce
+Solr mapreduce indexer
+
+%files -n solr-mapreduce
+%defattr(644,root,root,755)
+/usr/lib/solr/contrib
