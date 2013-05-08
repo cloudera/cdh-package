b1e7e34c7527bfa53a393e674627f61dfd744988
Roman Shaposhnik <rvs@cloudera.com>
2013-05-08 11:36:37 -0700

    KITCHEN-BUILD. Whirr CDH specific changes

--- bigtop-packages/src/common/whirr/install_whirr.sh
+++ bigtop-packages/src/common/whirr/install_whirr.sh
@@ -124,10 +124,14 @@ mv $LIB_DIR/docs/* $DOC_DIR
 mv $LIB_DIR/{NOTICE.txt,LICENSE.txt,BUILD.txt,CHANGES.txt,doap_Whirr.rdf,README.txt} $DOC_DIR
 
 # Remove some bits which sould not be shipped.
-for dir in src services pom.xml patch-stamp examples debian core cli build-tools bigtop-empty docs 
+for dir in src services pom.xml patch-stamp examples debian core cli build-tools bigtop-empty docs cloudera/patches cloudera/apply-patches cloudera/build.properties cloudera/CHANGES.cloudera.txt 'whirr*tar.gz'
 do
   rm -rf $LIB_DIR/$dir
 done
 
 # leave a doc symlink in place for compat. reasons
 ln -s /${DOC_DIR/#$PREFIX/} $LIB_DIR/docs
+
+# Cloudera specific
+install -d -m 0755 $LIB_DIR/cloudera
+cp cloudera/cdh_version.properties $LIB_DIR/cloudera/
