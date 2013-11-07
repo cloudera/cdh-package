a425af5fa57bdef67aa493dc162ef1e732eb0bfd
Sean Mackrory <sean@cloudera.com>
2013-11-07 10:19:38 -0800

    Adding Parquet-Format

---
+++ bigtop-packages/src/common/parquet-format/install_parquet-format.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+usage() {
+  echo "
+usage: $0 <options>
+  Required not-so-options:
+     --build-dir=DIR             path to parquet-format tarball with binaries
+     --prefix=PREFIX             path to install into
+
+  Optional options:
+     --lib-dir=DIR               path to install parquet-format jar files
+     ... [ see source for more similar options ]
+  "
+  exit 1
+}
+
+OPTS=$(getopt \
+  -n $0 \
+  -o '' \
+  -l 'prefix:' \
+  -l 'lib-dir:' \
+  -l 'build-dir:' -- "$@")
+
+if [ $? != 0 ] ; then
+    usage
+fi
+
+eval set -- "$OPTS"
+while true ; do
+    case "$1" in
+        --prefix)
+        PREFIX=$2 ; shift 2
+        ;;
+        --build-dir)
+        BUILD_DIR=$2 ; shift 2
+        ;;
+        --lib-dir)
+        LIB_DIR=$2 ; shift 2
+        ;;
+        --)
+        shift ; break
+        ;;
+        *)
+        echo "Unknown option: $1"
+        usage
+        exit 1
+        ;;
+    esac
+done
+
+for var in PREFIX BUILD_DIR ; do
+  if [ -z "$(eval "echo \$$var")" ]; then
+    echo Missing param: $var
+    usage
+  fi
+done
+
+LIB_DIR=${LIB_DIR:-/usr/lib/parquet}
+HADOOP_HOME=${HADOOP_HOME:-/usr/lib/hadoop}
+RELATIVE_PATH='../parquet' # LIB_DIR relative to HADOOP_HOME
+
+# First we'll move everything into lib
+install -d -m 0755 $PREFIX/$LIB_DIR
+install -d -m 0755 $PREFIX/$HADOOP_HOME
+for jar in `find $BUILD_DIR -name *.jar | grep -v '\-tests.jar'`; do
+    cp $jar $PREFIX/$LIB_DIR/
+    symlink=$PREFIX/$HADOOP_HOME/`basename $jar`
+    if [ ! -e $symlink ] ; then
+        ln -s $RELATIVE_PATH/`basename $jar` $symlink
+    fi
+done
+
+# Cloudera specific
+install -d -m 0755 $PREFIX/$LIB_DIR/parquet-format/cloudera
+cp cloudera/cdh_version.properties $PREFIX/$LIB_DIR/parquet-format/cloudera/
+
---
+++ bigtop-packages/src/rpm/parquet-format/SPECS/parquet-format.spec
@@ -0,0 +1,58 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+%define lib_parquet /usr/lib/parquet
+%define hadoop_home /usr/lib/hadoop
+
+# disable repacking jars
+%define __os_install_post %{nil}
+
+Name: parquet-format
+Version: %{parquet_format_version}
+Release: %{parquet_format_release}
+Summary: Format definitions for Parquet
+URL: http://parquet.io
+Group: Development/Libraries
+BuildArch: noarch
+Buildroot: %(mktemp -ud %{_tmppath}/parquet-format-%{version}-%{release}-XXXXXX)
+License: ASL 2.0 
+Source0: parquet-format-%{parquet_format_patched_version}.tar.gz
+Source1: do-component-build 
+Source2: install_parquet-format.sh
+Requires: hadoop
+Requires: parquet >= 1.2.3
+
+%description
+Format definitions for Parquet
+
+%prep
+%setup -n parquet-format-%{parquet_format_patched_version}
+
+%build
+bash $RPM_SOURCE_DIR/do-component-build
+
+%install
+%__rm -rf $RPM_BUILD_ROOT
+sh $RPM_SOURCE_DIR/install_parquet-format.sh \
+          --build-dir=./ \
+          --prefix=$RPM_BUILD_ROOT
+
+#######################
+#### FILES SECTION ####
+#######################
+%files 
+%defattr(-,root,root,755)
+%{lib_parquet}
+%{hadoop_home}/*.jar
