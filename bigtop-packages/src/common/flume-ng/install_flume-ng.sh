4ecaccfa32559eb53ab82b835ac64f24b4687b2c
Sean Mackrory <sean@cloudera.com>
2013-12-16 14:48:16 -0700

    CDH-16204, CDH-16255. Removing timestamps from Avro symlinks as well as versions

--- bigtop-packages/src/common/flume-ng/install_flume-ng.sh
+++ bigtop-packages/src/common/flume-ng/install_flume-ng.sh
@@ -175,11 +175,12 @@ cp cloudera/cdh_version.properties $PREFIX/$FLUME_DIR/cloudera/
 # Replace every Avro jar with a symlink to the versionless symlinks in our Avro distribution
 # This regex matches upstream versions, plus CDH versions, betas and snapshots if they are present
 versions='s#-[0-9].[0-9].[0-9]\(-cdh[0-9\-\.]*\)\?\(-beta-[0-9]\+\)\?\(-SNAPSHOT\)\?##'
+timestamps='s#-[0-9]\{8\}\.[0-9]\{6\}-[0-9]\{2\}##'
 for dir in $PREFIX/$FLUME_DIR/lib; do
     for old_jar in `ls $dir/avro-*.jar` ; do
         # Our Avro distribution does not include Cassandra or test JARs and we should remove them from the rest of CDH
         if [[ "$old_jar" =~ "-cassandra" || "$old_jar" =~ "-tests" ]] ; then continue; fi
-        new_jar=`echo \`basename $old_jar\` | sed -e $versions`
+        new_jar=`echo \`basename $old_jar\` | sed -e $versions | sed -e $timestamps`
         rm $old_jar && ln -fs /usr/lib/avro/$new_jar $dir/
     done
 done
--- bigtop-packages/src/common/oozie/install_oozie.sh
+++ bigtop-packages/src/common/oozie/install_oozie.sh
@@ -227,11 +227,12 @@ cp ${BUILD_DIR}/cloudera/cdh_version.properties ${SERVER_LIB_DIR}/cloudera/
 # Replace every Avro jar with a symlink to the versionless symlinks in our Avro distribution
 # This regex matches upstream versions, plus CDH versions, betas and snapshots if they are present
 versions='s#-[0-9].[0-9].[0-9]\(-cdh[0-9\-\.]*\)\?\(-beta-[0-9]\+\)\?\(-SNAPSHOT\)\?##'
+timestamps='s#-[0-9]\{8\}\.[0-9]\{6\}-[0-9]\{2\}##'
 for dir in ${SERVER_LIB_DIR}/libtools ${SERVER_LIB_DIR}/libserver ; do
     for old_jar in `ls $dir/avro-*.jar` ; do
         # Our Avro distribution does not include Cassandra or test JARs and we should remove them from the rest of CDH
         if [[ "$old_jar" =~ "-cassandra" || "$old_jar" =~ "-tests" ]] ; then continue; fi
-        new_jar=`echo \`basename $old_jar\` | sed -e $versions`
+        new_jar=`echo \`basename $old_jar\` | sed -e $versions | sed -e $timestamps`
         rm $old_jar && ln -fs /usr/lib/avro/$new_jar $dir/
     done
 done
