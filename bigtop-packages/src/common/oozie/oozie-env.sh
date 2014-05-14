d0af144394db2d7325a4724b045ed093e33cdfc9
Sean Mackrory <sean@cloudera.com>
2014-05-14 08:20:57 -0600

    CDH-17844. Oozie server isn't using libhadoop

--- bigtop-packages/src/common/oozie/oozie-env.sh
+++ bigtop-packages/src/common/oozie/oozie-env.sh
@@ -15,6 +15,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+export JAVA_LIBRARY_PATH="$JAVA_LIBRARY_PATH:/usr/lib/hadoop/lib/native"
+
 export OOZIE_CONFIG=/etc/oozie/conf
 export OOZIE_DATA=/var/lib/oozie
 export OOZIE_LOG=/var/log/oozie
