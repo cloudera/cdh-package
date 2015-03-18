df8cbcd0e2c7695979788629c239b17d511fad89
Rajendra Gokhale <rvg@cloudera.com>
2015-03-18 11:40:51 -0700

    CDH-21567 Link spark-assembly.jar directly to the versionfull spark-assembly-*.jar instead of doing so indirectly through versionless symlink

--- bigtop-packages/src/common/spark/install_spark.sh
+++ bigtop-packages/src/common/spark/install_spark.sh
@@ -268,7 +268,8 @@ install -d -m 0755 $PREFIX/$LIB_DIR/assembly/lib
 install -d -m 0755 $PREFIX/$LIB_DIR/examples/lib
 
 pushd $PREFIX/$LIB_DIR/assembly/lib
-ln -s ../../lib/spark-assembly*.jar .
+ln -s ../../lib/spark-assembly-*.jar .
+ln -s ../../lib/spark-assembly-*.jar ./spark-assembly.jar
 popd
 
 pushd $PREFIX/$LIB_DIR/examples/lib
