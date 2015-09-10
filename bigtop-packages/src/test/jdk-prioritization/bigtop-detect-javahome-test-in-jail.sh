2f45cad89cdc53b8d835e9d716dd5185b6233208
Rajendra Gokhale (centos6ec2.vpc.cloudera.com) <rvg@cloudera.com>
2015-09-10 14:51:41 -0700

    CDH-31162 Drop support for Java6 in bigtop-detect-java-home

--- bigtop-packages/src/test/jdk-prioritization/bigtop-detect-javahome-test-in-jail.sh
+++ bigtop-packages/src/test/jdk-prioritization/bigtop-detect-javahome-test-in-jail.sh
@@ -4,15 +4,16 @@
 JDKs='/usr/java/jdk1.7.0_67-cloudera
     /usr/java/jdk1.7.0_67
     /usr/java/jdk1.7.0_66
-    /usr/java/jdk1.6.0_31
     /usr/lib/jvm/jre-openjdk
     /usr/lib/jvm/java-1.7.0-openjdk-1.7.0.75.x86_64
-    /usr/lib/jvm/java-1.6.0-openjdk-1.6.0.34.x86_64
-    /usr/lib/jvm/jre-1.6.0-openjdk.x86_64
     /usr/lib/jvm/java-1.8.0-openjdk-1.8.0.31-1.b13.el6_6.x86_64'
 
-# For each path, create it and install a 'java' executable that would pass bigtop-detect-javahome's test
-for JDK in ${JDKs}; do
+BLACKLIST='/usr/java/jdk1.6.0_31
+    /usr/lib/jvm/java-1.6.0-openjdk-1.6.0.34.x86_64
+    /usr/lib/jvm/jre-1.6.0-openjdk.x86_64'
+
+# For each path, create it and install a 'java' executable 
+for JDK in ${JDKs} ${BLACKLIST} ; do
     mkdir -p ${JDK}/bin
     touch ${JDK}/bin/java
 done
@@ -28,5 +29,15 @@ for JDK in ${JDKs}; do
     rm -r ${JDK}
 done
 
+#Make sure that we do not use blacklisted versions of Java
+for JDK in ${BLACKLIST} ; do
+    unset JAVA_HOME
+    source /bin/bigtop-detect-javahome
+    if [ "${JAVA_HOME}" == ${JDK} ]; then
+        echo "${JAVA_HOME} set to a black-listed version of Java ${JDK}"
+        exit 1
+    fi
+    rm -r ${JDK}
+done
 exit 0
 
