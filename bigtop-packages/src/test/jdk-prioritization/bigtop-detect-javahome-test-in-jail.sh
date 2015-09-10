6f13795f88534fb1dc1f00c8d5370e7df6fd9e64
Rajendra Gokhale (centos6ec2.vpc.cloudera.com) <rvg@cloudera.com>
2015-09-10 14:09:45 -0700

    Modified bigtop-detect-javahome-test-in-jail.sh to fit the change to prefer JAVA8 over JAVA6 for CDH-31162

--- bigtop-packages/src/test/jdk-prioritization/bigtop-detect-javahome-test-in-jail.sh
+++ bigtop-packages/src/test/jdk-prioritization/bigtop-detect-javahome-test-in-jail.sh
@@ -4,12 +4,12 @@
 JDKs='/usr/java/jdk1.7.0_67-cloudera
     /usr/java/jdk1.7.0_67
     /usr/java/jdk1.7.0_66
-    /usr/java/jdk1.6.0_31
     /usr/lib/jvm/jre-openjdk
     /usr/lib/jvm/java-1.7.0-openjdk-1.7.0.75.x86_64
+    /usr/lib/jvm/java-1.8.0-openjdk-1.8.0.31-1.b13.el6_6.x86_64
+    /usr/java/jdk1.6.0_31
     /usr/lib/jvm/java-1.6.0-openjdk-1.6.0.34.x86_64
-    /usr/lib/jvm/jre-1.6.0-openjdk.x86_64
-    /usr/lib/jvm/java-1.8.0-openjdk-1.8.0.31-1.b13.el6_6.x86_64'
+    /usr/lib/jvm/jre-1.6.0-openjdk.x86_64'
 
 # For each path, create it and install a 'java' executable that would pass bigtop-detect-javahome's test
 for JDK in ${JDKs}; do
